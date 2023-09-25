da37f98536a2498320e29aba2f4bba732f45b10b
Lorenassantos <lorena.github@gmail.com>
2023-09-25 15:15:25 -0300

    arquivos k6

---
+++ tests/testeRotaGet.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export default function () {
+  const resultado = http.get('http://localhost:3333');
+
+  check (resultado, {
+    'status should be 200': (r) => r.status === 200
+  })
+
+  sleep(1);
+}
+
